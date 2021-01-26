ddbf2e60af9089b8dfee7315251afd8b76dd3ffb
Dillon <dillon@Dillons-MacBook-Pro.local>
2021-01-25 22:40:57 -0700

    Added part 2

--- part_2_defining_your_own_units.py
+++ part_2_defining_your_own_units.py
@@ -28,9 +28,13 @@ ppf = ppf.to(ur.gigapixels / ur.joules)
 print('CPU energy efficiency (converted):', ppf)
 print()
 
+pixel_size = 2 * ur.bytes / ur.pixel
+print('Bytes per pixel:', pixel_size)
+
 # Define a new unit: "op", a unit of
 # computation
 ur.define('op = [computation]')
 
-pixel_size = 2 * ur.bytes / ur.pixel
-print('Bytes per pixel:', pixel_size)
+arithmetic_intensity = 12 * ur.ops / ur.byte
+
+print('Arithmetic intensity of SEF:', arithmetic_intensity)
